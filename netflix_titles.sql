ff4cea5d8bbbec51e957ef614ba191418525d99c
omkarkhot8788 <153823178+omkarkhot8788@users.noreply.github.com>
2024-05-02 15:16:11 +0530

    netflix_titles.sql

---
+++ netflix_titles.sql
@@ -0,0 +1,16 @@
+CREATE TABLE netflix_titles ( id VARCHAR(50)
+,title VARCHAR(500)
+,type VARCHAR(10)
+,description VARCHAR(2000)
+,release_year INT
+,age_certification VARCHAR(500)
+,runtime VARCHAR(500)
+,genres VARCHAR(500)
+,production_countries VARCHAR(500)
+,seasons VARCHAR(500)
+,imdb_id decimal(3,1)
+,imdb_score VARCHAR(500)
+,imdb_votes INT
+,tmdb_popularity decimal(7,4)
+,tmdb_score decimal(7,4)
+);
